4a07fe04532d71ce261d16e10a8b54a3bb5a5a2b
Adam Donaghy <donaghyam@gmail.com>
2022-02-28 15:19:13 -0600

    done through chapter 7

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,5 +1,7 @@
+import React from "react"
 import { CustomerList } from "./customers/CustomerList.js"
 import { EmployeeList } from "./employees/EmployeeList.js"
+import { TicketList } from "./serviceTickets/ticketList.js"
 
 export const Repairs = () => {
     return (
@@ -11,6 +13,9 @@ export const Repairs = () => {
              
              <h2>Employee List</h2>
              <EmployeeList />
+
+             <h2>Service Tickets</h2>
+             <TicketList />
         </>
     )
 }
